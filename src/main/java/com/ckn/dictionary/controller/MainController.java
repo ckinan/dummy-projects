08a1eb8644e27a57e48fabd3a700d554ce00ae2a
Cesar <cesar.kina@gmail.com>
2018-02-25 19:18:04 -0500

    removing prism import

--- src/main/java/com/ckn/dictionary/controller/MainController.java
+++ src/main/java/com/ckn/dictionary/controller/MainController.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import com.ckn.dictionary.model.Record;
 import com.ckn.dictionary.model.RecordRepository;
-import com.sun.prism.impl.Disposer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -13,8 +12,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/")
